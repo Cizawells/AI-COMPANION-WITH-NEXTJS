21efc91cc398d983f771f31d428908ebc4947522
Cizawells <cizawells70@gmail.com>
2023-09-17 20:48:07 +0200

    changes

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -12,6 +12,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from ".
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { Wand2 } from "lucide-react";
+import axios from "axios"
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -71,7 +72,17 @@ const CompanionForm = ({ categories, initialData }: CompanionFormProps) => {
     const isLoading = form.formState.isSubmitting
  
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            if(initialData) {
+                //Update Companion functionality
+                await axios.patch(`/api/companion/${initialData.id}`, values)
+            }else {
+                //Create companion functionality
+                        await axios.post("/api/companion", values)
+            }
+        } catch (error) {
+            console.log(error, "SOMETHING WENT WRONG")
+        }
     }
   return (
    <div className="h-full p4 space-y-2 max-w-3xl mx-auto">
