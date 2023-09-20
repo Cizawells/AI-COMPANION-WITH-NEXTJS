50d03adbb2fb510a10f73fe2a2e122ec55bd6d72
ciza Wells <cizawells70@gmail.com>
2023-09-20 19:55:24 +0300

    added display of list of companions

--- components/Companions.tsx
+++ components/Companions.tsx
@@ -1,4 +1,7 @@
 import { Companion } from '@prisma/client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Card, CardHeader } from './ui/card'
 
 interface CompanionProps {
     data: (Companion & {
@@ -10,7 +13,28 @@ _count:  {
 
 const Companions = ({data}: CompanionProps) => {
   return (
-    <div>Companions</div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:gri-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10">
+
+      {data.map(item => (
+<Card key={item.id}
+className='bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0'>
+          <Link href={`/chat/${item.id}`}>
+            <CardHeader className='flex items-center justify-center text-center text-muted-foreground '>
+              <div className="relative w-32 h-32">
+                <Image 
+                  src={item.src}
+                  fill
+                  className="rounded-xl object-cover"
+                  alt="Companion"
+                />
+              </div>
+            </CardHeader>
+          
+          </Link>
+      </Card>
+      ))}
+      
+    </div>
   )
 }
 
